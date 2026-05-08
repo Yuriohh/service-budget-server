a5a6f30e5c195abe72dfc63f097f9345868e8400
Yuri Santos de Lima <yuri@redeparcerias.com>
2026-05-08 10:24:38 -0300

    fix: remove unused error variable in catch

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,9 +1,9 @@
 import { FastifyReply, FastifyRequest } from "fastify"
 
 export async function authMiddleware(request: FastifyRequest, reply: FastifyReply) {
-    try {
-        await request.jwtVerify()
-    } catch (error) {
-        reply.status(401).send({error: 'Unauthorized'})
-    }
+  try {
+    await request.jwtVerify()
+  } catch {
+    reply.status(401).send({ error: 'Unauthorized' })
+  }
 }
